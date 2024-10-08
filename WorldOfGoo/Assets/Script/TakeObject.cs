b66b7e15271321834dff42650099737e3c8ffb8c
lucasdprx <150352145+lucasdprx@users.noreply.github.com>
2024-10-08 21:35:08 +0200

    Fix bug link

--- WorldOfGoo/Assets/Script/TakeObject.cs
+++ WorldOfGoo/Assets/Script/TakeObject.cs
@@ -5,19 +5,26 @@ using UnityEngine.InputSystem;
 public class TakeObject : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+
     private GameObject _objectTake;
     private bool _isTake = false;
+
     [SerializeField] private float _radiusSphere;
     private Collider2D[] node;
+
     [SerializeField] private GameObject prefabLink;
     void Update()
     {
         if (_isTake)
         {
-            Vector3 position = _camera.ScreenToWorldPoint(Input.mousePosition);
-            position.z = 0;
-            _objectTake.transform.position = position;
+            FollowMouse();
         }
+    }
+    public void FollowMouse()
+    {
+        Vector3 position = _camera.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        _objectTake.transform.position = position;
     }
 
     public void LeftClick(InputAction.CallbackContext ctx)
@@ -25,8 +32,16 @@ public class TakeObject : MonoBehaviour
         if (ctx.performed)
         {
             RaycastHit2D hit = Physics2D.Raycast(_camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit.collider != null && hit.collider.gameObject.layer == 6 &&
-                hit.collider.GetComponent<MoveMonsters>() != null && hit.collider.GetComponent<MoveMonsters>().enabled)
+
+            if (hit.collider == null || hit.collider.gameObject.layer != 6)
+                return;
+
+            if (hit.collider.GetComponent<MoveMonsters>() == null)
+            {
+                return;
+            } 
+            
+            if (hit.collider.GetComponent<MoveMonsters>().enabled)
             {
                 hit.collider.GetComponent<MoveMonsters>().enabled = false;
                 _objectTake = hit.collider.gameObject;
@@ -46,24 +61,33 @@ public class TakeObject : MonoBehaviour
             _isTake = false;
         }
     }
+
+    private void ResetObject()
+    {
+        MoveMonsters moveMonsters = _objectTake.GetComponent<MoveMonsters>();
+        _objectTake.transform.position = moveMonsters._currentNode.position;
+        moveMonsters.enabled = true;
+    }
     private void AddLink()
     {
         Vector3 position = _camera.ScreenToWorldPoint(Input.mousePosition);
         position.z = 0;
         node = Physics2D.OverlapCircleAll(position, _radiusSphere);
+        SortCollider(node, _objectTake.transform.position);
+
         List<Collider2D> closestNode = new();
         for (int i = 0; i < node.Length; i++)
         {
-            if (node[i].TryGetComponent(out Link link) && link._links.Count > 0 && closestNode.Count < 2)
+            Collider2D[] verif = Physics2D.OverlapCircleAll(position, 0.5f);
+            if (node[i].TryGetComponent(out Link link) && link._links.Count > 0 && closestNode.Count < 2 && verif.Length < 2)
             {
                 closestNode.Add(node[i]);
             }
         }
+
         if (closestNode.Count < 2)
         {
-            MoveMonsters moveMonsters = _objectTake.GetComponent<MoveMonsters>();
-            _objectTake.transform.position = moveMonsters._currentNode.position;
-            moveMonsters.enabled = true;
+            ResetObject();
             return;
         }
 
@@ -73,17 +97,36 @@ public class TakeObject : MonoBehaviour
             _objectTake.GetComponent<Link>()._links.Add(closestNode[i].transform);
             closestNode[i].GetComponent<Link>()._links.Add(_objectTake.transform);
 
-            //Set position and scale
-            Vector3 newPos = (_objectTake.transform.position - closestNode[i].transform.position) / 2 + closestNode[i].transform.position;
-            GameObject link = Instantiate(prefabLink);
-            link.transform.position = newPos;
-            link.transform.localScale = new Vector3(Vector2.Distance(closestNode[i].transform.position, _objectTake.transform.position),
-                                                    link.transform.localScale.y, 1);
+            CreateLink(closestNode[i]);
+        }
+    }
+    private void CreateLink(Collider2D closestNode)
+    {
+        //Set position and scale
+        Vector3 newPos = (_objectTake.transform.position - closestNode.transform.position) / 2 + closestNode.transform.position;
+        GameObject link = Instantiate(prefabLink);
+        link.transform.position = newPos;
+        float scaleX = Vector2.Distance(closestNode.transform.position, _objectTake.transform.position) - link.transform.localScale.x;
+        link.transform.localScale += new Vector3(scaleX,0, 0);
 
-            //Rotate
-            Vector3 dir = _objectTake.transform.position - link.transform.position;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            link.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, link.transform.rotation.y, 1));
+        //Rotate
+        Vector3 dir = _objectTake.transform.position - link.transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        link.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, link.transform.rotation.y, 1));
+    }
+
+    private void SortCollider(Collider2D[] node, Vector2 posObj)
+    {
+        for (int i = 0; i < node.Length - 1; i++)
+        {
+            for (int j = 0; j < node.Length - 1; j++)
+            {
+                if (Vector2.Distance(posObj, node[j].transform.position) >
+                    Vector2.Distance(posObj, node[j + 1].transform.position))
+                {
+                    (node[j + 1], node[j]) = (node[j], node[j + 1]);
+                }
+            }
         }
     }
 }
